7267e40d23004739dab0e5c7bb1accc8b0f7e270
farhanraa <farhanraa@gmail.com>
2017-10-21 21:07:19 +0700

    lab06

---
+++ apaptutorial06/src/main/java/com/example/apaptutorial05/Apaptutorial05Application.java
@@ -0,0 +1,12 @@
+package com.example.apaptutorial05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Apaptutorial05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Apaptutorial05Application.class, args);
+	}
+}
